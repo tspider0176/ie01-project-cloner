0ae95bd5d3dd16eac88d817646f8c112ee498930
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2018-04-20 18:12:38 +0900

    Implement get all team on classroom from GitHub API

--- corrector.rb
+++ corrector.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'json'
 
 roster_csv = CSV.read('classroom_roster.csv', headers: false)
 mailing_list_csv = CSV.read('11-3002-IE03-std5.csv', headers: false)
@@ -17,5 +18,21 @@ classroom_roster = all_student.drop(1).select do |item|
   classroom.map(&:first).include?(item.first.split('_').first)
 end
 
-puts 'student ID, name and GitHub ID for std5'
+# output student ID, name and GitHub ID for std5
 puts classroom_roster.reject { |arr| arr[1] == '' }.map { |item| item.join(',') }.join("\n")
+
+# get TOKEN from TOKEN file
+token = ''
+File.open('./TOKEN', 'r') do |f|
+  token = f.read
+end
+
+# Get by GitHub API
+`curl -o out.json -v -H "Authorization: token #{token}" https://api.github.com/orgs/ie03-aizu/teams`
+
+hash = {}
+# Get all teams from out.json
+File.open('out.json', 'r') do |f|
+  hash = JSON.parse(f.read)
+end
+
