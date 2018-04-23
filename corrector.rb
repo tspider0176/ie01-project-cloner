165b889ba4ac7468b7b91e7197ec016ece553148
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2018-04-23 17:57:14 +0900

    Refactoring corrector.rb

--- corrector.rb
+++ corrector.rb
@@ -3,45 +3,32 @@ require 'json'
 
 EP = 'https://api.github.com/'.freeze
 
+# get TOKEN from TOKEN file
+token = ''
+File.open('./TOKEN', 'r') do |f|
+  token = f.read
+end
+TOKEN = "\"Authorization: token #{token}\"".freeze
+
 all_student = []
 classroom = []
 unless ARGV[0].nil? || ARGV[1].nil?
-  mailing_list_csv = CSV.read(ARGV[0], headers: false)
-  roster_csv = CSV.read(ARGV[1], headers: false)
-
-  roster_csv.each do |data|
+  CSV.read(ARGV[1], headers: false).each do |data|
     all_student.push(data)
   end
 
-  mailing_list_csv.each do |data|
+  CSV.read(ARGV[0], headers: false).each do |data|
     classroom.push(data)
   end
-end
-
-classroom_roster = all_student.drop(1).select do |item|
-  classroom.map(&:first).include?(item.first.split('_').first)
-end
-
-# output student ID, name and GitHub ID for inputted class
-puts classroom_roster.reject { |arr| arr[1] == '' }.map { |item| item.join(',') }.join("\n")
-
-github_id_list = classroom_roster.reject { |arr| arr[1] == '' }.map do |elem|
-  elem[1]
 end
 
 puts 'Update team list?:(y/n)'
 printf '> '
 input = gets.chomp
 
-if input != 'n'
-  # get TOKEN from TOKEN file
-  token = ''
-  File.open('./TOKEN', 'r') do |f|
-    token = f.read
-  end
-
+if input == 'y'
   # Get by GitHub API
-  `curl -o out.json -v -H "Authorization: token #{token}" #{EP}orgs/ie03-aizu/teams`
+  `curl -o out.json -H #{TOKEN} #{EP}orgs/ie03-aizu/teams`
 
   hash = {}
   # Get all teams from out.json
@@ -56,15 +43,19 @@ if input != 'n'
     item['id']
   end
 
-  # Get value all of member URL from key 'members_url'
+  # Get all of member URL from key 'members_url'
   id_list.each do |id|
-    puts 'getting members for each team ID...'
-    `curl -o #{id}_members.json -H "Authorization: token #{token}" #{EP}teams/#{id}/members`
+    `curl -o #{id}_members.json -H #{TOKEN} #{EP}teams/#{id}/members`
     `mv *.json ./temp/`
   end
 
   # Search teams which contains student belonging to std5
   target_teams = []
+  classroom_roster = all_student.drop(1).select do |item|
+    classroom.map(&:first).include?(item.first.split('_').first)
+  end
+
+  github_id_list = classroom_roster.reject { |arr| arr[1] == '' }.map { |elem| elem[1] }
   Dir.glob('./temp/*.json') do |file|
     teamid = File.basename(file).split('_').first
     File.open(file, 'r') do |f|
@@ -79,26 +70,21 @@ if input != 'n'
 
   # Get all repositories for each detected team
   target_team_ids.each do |team_id|
-    `curl -o #{team_id}_repos.json -H "Authorization: token #{token}" #{EP}teams/#{team_id}/repos`
+    `curl -o #{team_id}_repos.json -H #{TOKEN} #{EP}teams/#{team_id}/repos`
     `mv *json ./repos/`
   end
 end
 
 # Create repository URL list which should be cloned to local
-clone_urls = []
 Dir.glob('./repos/*.json') do |file|
   File.open(file, 'r') do |f|
     jsonarr = JSON.parse(f.read)
-    clone_urls.push(jsonarr.first['ssh_url'])
+    ssh_url = jsonarr.first['ssh_url']
+    puts '----------'
+    repository_name = File.basename(ssh_url)
+    `git clone #{ssh_url} ./student_projects/#{File.basename(repository_name, '.*')}`
   end
 end
 
 # remove exists local repos
 `rm -rf ./student_projects/*`
-
-# clone repositories to local
-clone_urls.each { |url|
-  puts '----------'
-  repository_name = File.basename(url)
-  `git clone #{url} ./student_projects/#{File.basename(repository_name, '.*')}`
-}
