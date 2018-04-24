e765208e7f74080a6e0becdd4fd172f7a08bd2f8
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2018-04-24 17:46:33 +0900

    Refactoring

--- corrector.rb
+++ corrector.rb
@@ -10,8 +10,8 @@ File.open('./TOKEN', 'r') do |f|
 end
 TOKEN = "\"Authorization: token #{token}\"".freeze
 
-def update(all_student, classroom)
-  # Get all teams from ie03-aizu organization
+# Get all team
+def all_team
   `curl -o out.json -H #{TOKEN} #{EP}orgs/ie03-aizu/teams?per_page=100`
 
   all_team = {}
@@ -22,12 +22,51 @@ def update(all_student, classroom)
 
   `rm out.json`
 
-  # Get all member URL from API
-  # After this op, temp/ dir contains all team member info.
+  all_team
+end
+
+# Store all team member data to temp/ dir
+def store_all_team_member
   all_team.map { |item| item['id'] }.each do |id|
     `curl -o #{id}_members.json -H #{TOKEN} #{EP}teams/#{id}/members`
     `mv *.json ./temp/`
   end
+end
+
+# Create GitHub ID list from classroom_roster csv file
+# Remove students who didn't have GitHub Name from the list
+def github_id_list(classroom_roster)
+  classroom_roster.reject { |arr| arr[1] == '' }.map { |elem| elem[1] }
+end
+
+def members?(team_users, classroom_roster)
+  team_users.select do |item|
+    github_id_list(classroom_roster).include?(item['login'])
+  end.empty?
+end
+
+def clone_target_teams(classroom_roster)
+  # Search teams which contains student who are in mailing list
+  target_teams = []
+  Dir.glob('./temp/*.json') do |file|
+    puts "Seeing #{file}..."
+    teamid = File.basename(file).split('_').first
+    File.open(file, 'r') do |f|
+      team_users = JSON.parse(f.read)
+      # if there exists students who are contained to github_id_list,
+      target_teams.push(teamid) unless members?(team_users, classroom_roster)
+    end
+  end
+  target_teams
+end
+
+def clone_target_team_ids(classroom_roster)
+  clone_target_teams(classroom_roster).map { |name| File.basename(name, '.*') }
+end
+
+def update(all_student, classroom)
+  # Get all team and its members to local
+  store_all_team_member
 
   # Drop header from classroom_roster.csv
   # Create a list which contains students on mailing list
@@ -36,35 +75,13 @@ def update(all_student, classroom)
     classroom.map(&:first).include?(student_id)
   end
 
-  # Remove students who didn't have GitHub Name from list
-  github_id_list = classroom_roster.reject { |arr| arr[1] == '' }.map do |elem|
-    elem[1]
-  end
-
-  # Search teams which contains student who are in mailing list
-  target_teams = []
-  Dir.glob('./temp/*.json') do |file|
-    puts "Seeing #{file}..."
-    teamid = File.basename(file).split('_').first
-    File.open(file, 'r') do |f|
-      team_users = JSON.parse(f.read)
-      # p team_users.map { |item| item['login'] }
-      # if there exists students who are contained to github_id_list
-      target_teams.push(teamid) unless team_users.select { |item| github_id_list.include?(item['login']) }.empty?
-    end
-  end
-
-  target_team_ids = target_teams.map { |name| File.basename(name, '.*') }
-
   # Get all repositories for each detected team
-  target_team_ids.each do |team_id|
+  clone_target_team_ids(classroom_roster).each do |team_id|
     `curl -o #{team_id}_repos.json -H #{TOKEN} #{EP}teams/#{team_id}/repos`
     `mv *json ./repos/`
   end
 end
 
-all_student = []
-classroom = []
 unless ARGV[0].nil? || ARGV[1].nil?
   CSV.read(ARGV[1], headers: false).each do |data|
     all_student.push(data)
@@ -73,9 +90,7 @@ unless ARGV[0].nil? || ARGV[1].nil?
   CSV.read(ARGV[0], headers: false).each do |data|
     classroom.push(data)
   end
-end
 
-unless ARGV[0].nil? || ARGV[1].nil?
   puts 'Update team list? (y/n)'
   print '> '
   input = STDIN.gets.chomp
@@ -86,9 +101,7 @@ end
 # remove old local repos
 `rm -rf ./student_projects/*`
 
-puts '=================================================='
-puts 'Clone all repositories to ./student_projects'
-puts '=================================================='
+puts "Clone all repo.s created by students in #{ARGV[0]} to ./student_projects"
 # Create repository URL list which should be cloned to local
 Dir.glob('./repos/*.json') do |file|
   File.open(file, 'r') do |f|
